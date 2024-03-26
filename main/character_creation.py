4388280379aa3273692cfb14fd7890fc240085e4
CJRoedel <cjroedel02@gmail.com>
2024-03-26 16:18:34 -0400

    Progress on character creation

--- main/character_creation.py
+++ main/character_creation.py
@@ -4,22 +4,29 @@
 #imports
 import csv
 
-def create():
-    character_values = ["name", "total_hp", "current_hp", "ac"]
+#globals
+character_fields = ["name", "total_hp", "current_hp", "ac"]
+
+def create_character(field_values):
+    next
+
+def value_inputs():
+    field_values = [None] * len(character_fields)
     save_character = True
     values_index = 0
 
-    while values_index < len(character_values):
+    while values_index < len(character_fields):
 
-        input_value = input("Please enter the ", character_values[values_index], " for this character: ")
+        input_value = input("\nPlease enter the " + character_fields[values_index] +" for this character: ")
         input_value = input_value.lower()
 
         if input_value == "cancel":
-                print("Are you sure you want to cancel creating this character? Nothing will be saved.\n" + 
+                print("\nAre you sure you want to cancel creating this character? Nothing will be saved.\n" + 
                       "Enter 'yes' to cancel or anything else to continue.")
                 
                 cancel_input = input("\nCancel? ")
                 cancel_input.lower()
+                print("")
 
                 if cancel_input == "yes":
                     save_character = False
@@ -28,7 +35,27 @@ def create():
                 else:
                     continue
         else:
-            print("else")
+            if character_fields[values_index] != "name":
+                if input_value.isnumeric():
+                    print("Character " + character_fields[values_index] + " saved as " + input_value)
+                    field_values[values_index] = input_value
+                    values_index += 1
+                else:
+                    print("Please only input integers for values that are not names.")
+            else:
+                if input_value.isalnum():
+                    print("Character " + character_fields[values_index] + " saved as " + input_value)
+                    field_values[values_index] = input_value
+                    values_index += 1
+                else:
+                    print("Please only input letters and digits for a characters name.")
+    
+    print("\nCharacter completed. Here is your character: ")
+    for i in range(len(character_fields)):
+        print("Character " + character_fields[i] + ": " + field_values[i])
+    print("\nIf you would like to edit your character enter 'edit'. If you are satisfied\n" + 
+          "with your current character enter anything else.")
+
             
 
 def upload_characters():
@@ -51,7 +78,7 @@ def upload_characters():
 
         match character_command:
             case "create":
-                create()
+                value_inputs()
             case "exit":
                 print("Mode changed from character upload to menu.")
                 creation_running = False
